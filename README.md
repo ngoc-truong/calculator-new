c50b8962f07d791d99bdf3c55536e2c1f835de59
Ngoc Truong <ngoc.truong@posteo.de>
2020-04-24 22:26:07 +0200

    Finish design

---
+++ README.md
@@ -0,0 +1,5 @@
+# Calculator
+
+In this project I created a calculator with HTML, CSS and JavaScript. This project
+is part of the online curriculum [The Odin Project](https://www.theodinproject.com/courses/web-development-101/lessons/calculator?ref=lnav).
+
---
+++ main.js
@@ -0,0 +1,36 @@
+// Operations
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function substract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(operator, num1, num2) {
+    switch(operator){
+        case "plus":
+            return add(num1, num2);
+            break;
+        case "minus":
+            return add(num1, num2);
+            break;
+        case "multiply":
+            return multiply(num1, num2);
+            break;
+        case "divide":
+            return divide(num1, num2);
+            break;
+        default:
+            
+    }
+}
